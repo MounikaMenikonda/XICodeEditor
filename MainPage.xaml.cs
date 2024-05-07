d755cdc17d028e408246e411daa1b665f07fcb24
MounikaMenikonda <94614487+MounikaMenikonda@users.noreply.github.com>
2024-05-07 11:04:54 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace XICodeEditor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+    public string Get_HoverContent()
+    {
+        try
+        {
+
+            return "text";
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+    public string Get_ContractContent(int ID, string boName)
+    {
+        try
+        {
+
+            return "ContractConten";
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+}
